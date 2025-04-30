4712d6377687ac40ccdfc2d71d0a3856c08fc0bf
daichi takei <dadewujing0@gmail.com>
2025-04-30 20:02:33 +0900

    update: データがないときにメッセージを出す

--- src/components/AllReflection.tsx
+++ src/components/AllReflection.tsx
@@ -5,28 +5,34 @@ export const AllReflection = (props: unknown) => {
         <div className="text-white bg-blue-600 w-50 text-center rounded-xl mb-3">
           これまでの振り返り
         </div>
-        {props.allcomments.map((one_comment, index) => (
-          <div className="max-w-290 border-2 bg-gray-100 border-gray-300 mx-auto rounded-xl mb-2 p-3">
-            <div key={index}>
-              <div className="me-5 text-xs">
-                {new Date(one_comment.created_at).toLocaleString("ja-JP", {
-                  timeZone: "Asia/Tokyo",
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                  hour: "numeric",
-                  minute: "numeric",
-                })}
-              </div>
-              <div className="text-3xl flex">
-                <div className="me-3">
-                  {"⭐️".repeat(Number(one_comment.rating))}
+        <div className="mx-5">
+          {props.allcomments.length == 0 ? "まだ振り返りはありません" : null}
+
+          {props.allcomments.map((one_comment, index) => (
+            <div>
+              <div className="max-w-290 border-2 bg-gray-100 border-gray-300 mx-auto rounded-xl mb-2 p-3">
+                <div key={index}>
+                  <div className="me-5 text-xs">
+                    {new Date(one_comment.created_at).toLocaleString("ja-JP", {
+                      timeZone: "Asia/Tokyo",
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                      hour: "numeric",
+                      minute: "numeric",
+                    })}
+                  </div>
+                  <div className="text-3xl flex">
+                    <div className="me-3">
+                      {"⭐️".repeat(Number(one_comment.rating))}
+                    </div>
+                    <div className="me-5">{one_comment.comment}</div>
+                  </div>
                 </div>
-                <div className="me-5">{one_comment.comment}</div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
